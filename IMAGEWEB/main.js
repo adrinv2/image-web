d0e7320f80ce563bbf8b5ea48cf666a23443b242
adrinv2 <55081904+adrinv2@users.noreply.github.com>
2020-05-27 13:17:44 +0200

    .

--- IMAGEWEB/main.js
+++
@@ -1,9 +0,0 @@
-
-// Do not name the function "play()"
-function playVideo(){
-    var video = document.getElementById('video');
-    video.play();
-    video.addEventListener('ended',function(){
-        window.location = "main.html";
-    });
-}
